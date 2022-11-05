d82721044ed130bc5442d3ce322e45e8f94024b5
elad aviv <elad.aviv@workiz.com>
2022-11-05 22:21:44 +0200

    update comment in the index router

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,12 @@ const productsRouter = require("./products.routes");
 const shopRouter = require("./cart.routes");
 const router = Router();
 
-router.get('/', (req, res) => res.redirect("/shop/home")); // fallback for homePage
+// fallback for homePage
+router.get('/', (req, res) => res.redirect("/shop/home"));
+
 router.use("/user", userRouter);
 router.use("/pages", pagesRouter);
 router.use("/products", productsRouter);
 router.use("/shop", shopRouter)
+
 module.exports = router;
