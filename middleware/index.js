cb23ebafdb7c1dc54b8ffcf2c591508b3944300d
shirbalas <shir29291@gmail.com>
2022-11-06 23:57:56 +0300

    add admin infrastructure

--- middleware/index.js
+++ middleware/index.js
@@ -15,4 +15,11 @@ middlewareObject.isLoggedIn = (req, res, next) => {
   res.redirect("/user/signin");
 };
 
+middlewareObject.isAdmin = (req, res, next) => {
+  if (!res.locals.currentUser?.userType === "Admin") {
+    next();
+  }
+  res.redirect("/");
+}
+
 module.exports = middlewareObject;
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ const userRouter = require("./user.routes");
 const pagesRouter = require("./pages.routes");
 const productsRouter = require("./products.routes");
 const shopRouter = require("./cart.routes");
+const { isAdmin } = require("../middleware/index");
 const router = Router();
 
 // fallback for homePage
 
-router.use("/user", userRouter);
+router.use("/user" ,userRouter);
 router.use("/pages", pagesRouter);
 router.use("/products", productsRouter);
 router.use("/shop", shopRouter)
