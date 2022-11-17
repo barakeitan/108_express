ce4c98347fda835bc8a87afb53c0069463effb40
Barak Eitan <barakeitan48@gmail.com>
2022-11-18 00:50:49 +0200

    Make the handle map locations function

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,23 @@
 function initMap() {
-    var test = {lat: 31.969956418658118, lng: 34.77279792642369}
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: test
-    });
-    var marker = new google.maps.Marker({
-      position: test,
-      map: map
+  var locs=[];
+    $.get('/pages/map-location', function(data, status) {
+      // locs = [{lat:data.location[0].lat, lng:data[.locatiosn0].lng}, {lat:data[1].lat, lng:data[1].lng}]
+      console.log("data is:", data);
+      console.log("data.locations is : ", data.locations)
+      for (let index = 0; index < data.locations.length; index++) {
+        const element = data.locations[index];
+        locs.push({lat:element.lat, lng:element.lng});
+      }
+      console.log("locs[0] : ", locs[0])
+      var map = new google.maps.Map(document.getElementById('map'),{
+        zoom: 10,
+        center: locs[0],
+      });
+      locs.forEach(marker => {
+        new google.maps.Marker({
+          position: marker,
+          map: map
+        });
+      });
     });
   }
