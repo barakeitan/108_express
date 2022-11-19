8aed7ad8d46d338549106377ba526c8a707f3f45
Barak Eitan <barakeitan48@gmail.com>
2022-11-19 22:54:37 +0200

    change the scatter graph to have real data

--- public/js/connected-scatter-graph.js
+++ public/js/connected-scatter-graph.js
@@ -1,59 +1,68 @@
 
+      const scatGraphWidth = 460, scatGraphHeight = 400;
+      
       // set the dimensions and margins of the graph
       var margin = {top: 10, right: 30, bottom: 30, left: 60},
-          width = 460 - margin.left - margin.right,
-          height = 400 - margin.top - margin.bottom;
+          width = scatGraphWidth - margin.left - margin.right,
+          height = scatGraphHeight - margin.top - margin.bottom;
       
       // append the svg object to the body of the page
       var svg2 = d3.select("#scatter-svg")
-      .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
+        .attr("width", scatGraphWidth)
+        .attr("height", scatGraphHeight)
         .append("g")
-          .attr("transform",
+        .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
       
-      //Read the data - Just testing!! 
-      d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/connectedscatter.csv",
-        // When reading the csv, I must format variables:
-        function(d){
-          return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
-        },
-        // Now I can use this dataset:
-        function(data) {
+      var scatterData;
+      $.get("/user/admin/scatter-data", (data, status) =>{scatterData = data; console.log("data in scatter : ", scatterData)});
 
-          // Add X axis --> it is a date format
-          var x = d3.scaleTime()
-            .domain(d3.extent(data, function(d) { return d.date; }))
-            .range([ 0, width ]);
-          svg2.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+      //Read the data - Just testing!! 
+      d3.json(scatterData, function(data) {
+
+          // Add X axis
+          var x = d3.scaleBand()
+          .range([ 0, width ])
+          .domain(scatterData.map(function(d) { return d.slug; }))
+          .padding(0.9);
+        svg2.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x))
+          .selectAll("text")
+            .attr("transform", "translate(-10,0)rotate(-45)")
+            .style("text-anchor", "end");
+
           // Add Y axis
           var y = d3.scaleLinear()
-            .domain( [8000, 9200])
-            .range([ height, 0 ]);
-          svg2.append("g")
-            .call(d3.axisLeft(y));
+          .domain([0, 20])
+          .range([ height, 0]);
+        svg2.append("g")
+          .call(d3.axisLeft(y));
+
           // Add the line
           svg2.append("path")
-            .datum(data)
+            .datum(scatterData)
             .attr("fill", "none")
             .attr("stroke", "#69b3a2")
             .attr("stroke-width", 1.5)
             .attr("d", d3.line()
-              .x(function(d) { return x(d.date) })
+              .x(function(d) { return x(d.slug) })
               .y(function(d) { return y(d.value) })
               )
+            .on("mouseover", function(){d3.select(this).style("stroke", "orange");})
+            .on("mouseout", function(){d3.select(this).style("stroke", "#69b3a2");})
           // Add the points
           svg2
             .append("g")
             .selectAll("dot")
-            .data(data)
+            .data(scatterData)
             .enter()
             .append("circle")
-              .attr("cx", function(d) { return x(d.date) } )
+              .attr("cx", function(d) { return x(d.slug) } )
               .attr("cy", function(d) { return y(d.value) } )
               .attr("r", 5)
               .attr("fill", "#69b3a2")
+            .on("mouseover", function(){d3.select(this).style("fill", "orange");})
+            .on("mouseout", function(){d3.select(this).style("fill", "#69b3a2");})
       })
      
